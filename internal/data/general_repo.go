59c10eb19340d6052e00b143668846b36efe1a8a
lukun1 <lukun1@tal.com>
2023-12-19 17:28:53 +0800

    添加通用db接口

--- internal/data/general_repo.go
+++ internal/data/general_repo.go
@@ -1,7 +1,7 @@
 package data
 
 import (
-	"babycare/internal/biz"
+	"babycare/internal/biz/common"
 	"context"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
@@ -15,8 +15,9 @@ func (g generalRepo) Transaction(ctx context.Context, fc func(tx *gorm.DB) error
 	return g.data.db.WithContext(ctx).Transaction(fc)
 }
 
-func (g generalRepo) Create(ctx context.Context, v any, scope ...func(tx *gorm.DB) *gorm.DB) error {
-	return g.data.db.WithContext(ctx).Scopes(scope...).Create(v).Error
+func (g generalRepo) Create(ctx context.Context, v any, scope ...func(tx *gorm.DB) *gorm.DB) (int64, error) {
+	tx := g.data.db.WithContext(ctx).Scopes(scope...).Create(v)
+	return tx.RowsAffected, tx.Error
 }
 
 func (g generalRepo) CreateInBatches(ctx context.Context, v any, size int) error {
@@ -27,20 +28,20 @@ func (g generalRepo) Get(ctx context.Context, dest schema.Tabler, id uint) error
 	return g.data.db.WithContext(ctx).First(dest, id).Error
 }
 
-func (g generalRepo) GetBy(ctx context.Context, dest schema.Tabler, scope biz.Scope) error {
+func (g generalRepo) GetBy(ctx context.Context, dest schema.Tabler, scope common.Scope) error {
 	return g.data.db.WithContext(ctx).Scopes(scope).First(dest).Error
 }
 
-func (g generalRepo) Find(ctx context.Context, dest any, scope biz.Scope) error {
+func (g generalRepo) Find(ctx context.Context, dest any, scope common.Scope) error {
 	return g.data.db.WithContext(ctx).Scopes(scope).Find(dest).Error
 }
 
-func (g generalRepo) Count(ctx context.Context, m schema.Tabler, scope biz.Scope) (count int64, err error) {
+func (g generalRepo) Count(ctx context.Context, m schema.Tabler, scope common.Scope) (count int64, err error) {
 	err = g.data.db.WithContext(ctx).Model(m).Scopes(scope).Count(&count).Error
 	return
 }
 
-func (g generalRepo) FindBy(ctx context.Context, dest any, scope biz.Scope, pager biz.Pagination) (count int64, err error) {
+func (g generalRepo) FindBy(ctx context.Context, dest any, scope common.Scope, pager common.Pagination) (count int64, err error) {
 	limit := int(pager.GetSize())
 	if limit < 1 {
 		limit = 10
@@ -58,7 +59,7 @@ func (g generalRepo) Update(ctx context.Context, m schema.Tabler, id uint, value
 	return g.data.db.WithContext(ctx).Model(m).Where("id = ?", id).Updates(values).Error
 }
 
-func (g generalRepo) UpdateBy(ctx context.Context, m schema.Tabler, scope biz.Scope, values map[string]any) error {
+func (g generalRepo) UpdateBy(ctx context.Context, m schema.Tabler, scope common.Scope, values map[string]any) error {
 	return g.data.db.WithContext(ctx).Model(m).Scopes(scope).Updates(values).Error
 }
 
@@ -66,11 +67,11 @@ func (g generalRepo) Delete(ctx context.Context, m schema.Tabler, id uint) error
 	return g.data.db.WithContext(ctx).Delete(m, id).Error
 }
 
-func (g generalRepo) DeleteBy(ctx context.Context, m schema.Tabler, scope biz.Scope) error {
+func (g generalRepo) DeleteBy(ctx context.Context, m schema.Tabler, scope common.Scope) error {
 	return g.data.db.WithContext(ctx).Scopes(scope).Delete(m).Error
 }
 
-func NewGeneralRepo(data *Data) biz.GeneralRepo {
+func NewGeneralRepo(data *Data) common.GeneralRepo {
 	return &generalRepo{
 		data: data,
 	}
